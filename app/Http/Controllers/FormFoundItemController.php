ea1dc35ee092918d1442e4e1a63c7279a9dd01c3
andikagr <andikagilangramadhan89@gmail.com>
2026-06-10 00:56:45 +0700

    Fix kontak_penemu validasi (numeric->string) dan fix upload gambar Base64

--- app/Http/Controllers/FormFoundItemController.php
+++ app/Http/Controllers/FormFoundItemController.php
@@ -22,14 +22,15 @@ class FormFoundItemController extends Controller
             'lokasi_penemuan' => 'required|string|max:255',
             'kronologi' => 'nullable|string',
             'nama_penemu' => 'required|string|max:255',
-            'kontak_penemu' => 'required|numeric',
+            'kontak_penemu' => 'required|string|max:255',
             'alamat_penemu' => 'required|string|max:255',
             'deskripsi' => 'required|string',
             'image' => 'nullable|image|max:2048',
         ]);
 
         if ($request->hasFile('image')) {
-            $data['image'] = $request->file('image')->store('found-items', 'public');
+            $file = $request->file('image');
+            $data['image'] = 'data:' . $file->getMimeType() . ';base64,' . base64_encode(file_get_contents($file->getRealPath()));
         }
 
         $data['kontak'] = $data['kontak_penemu'];
